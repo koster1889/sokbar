fc996ea57c2a74099b36707c0ff92f9961ceb0bd
Johannes <koster1889@gmail.com>
2016-08-18 22:22:02 +0200

    copy-paste from grease monkey script

---
+++ sokbar.js
@@ -0,0 +1,27 @@
+LKey = 76
+
+currentLocation = new URL(window.location).hostname
+
+searchElementFinder = {
+  "sv.wikipedia.org": function() {
+    return document.getElementById("searchInput");
+  },"en.wikipedia.org": function() {
+    return document.getElementById("searchInput");
+  } // More to be added as we go
+}
+
+if (currentLocation in searchElementFinder) {
+  finder = searchElementFinder[currentLocation]
+} else {
+  finder = function () {
+    // Standard stuff
+    return document.getElementsByName("q")[0];
+  }
+}
+
+window.onkeyup = function(e) {
+  var key = e.keyCode ? e.keyCode : e.which;
+  if (e.shiftKey && key == LKey && e.ctrlKey) {
+    finder().select();
+  }
+	}
